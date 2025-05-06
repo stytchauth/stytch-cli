d42eb81f59a3e2a014a94b71cd64e400b1b6988c
afauci-stytch <afauci@stytch.com>
2025-05-06 15:55:18 -0700

    Add install script (#6)
    * Add install script
    * Also fix lint issue

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+echo "Installing stytch-cli..."
+go install github.com/stytchauth/stytch-cli@latest
+
+# First check for $(go env GOBIN)
+gobin=$(go env GOBIN)
+
+# If that isn't set, use $(go env GOPATH)/bin
+if [ -z "$gobin" ]; then
+	gobin=$(go env GOPATH)/bin
+fi
+
+# If *that* isn't set, use $HOME/go/bin
+if [ -z "$gobin" ]; then
+	gobin=$HOME/go/bin
+fi
+
+# Get the user's default shell
+shell=$(basename $SHELL)
+echo "Adding shell completion for $shell..."
+
+# Run the completion script
+$gobin/stytch-cli completion $shell >$HOME/.stytch-cli-completion.$shell
+
+# Check if the user's rc file sources the completion file
+if ! grep -q ".stytch-cli-completion.$shell" $HOME/.${shell}rc; then
+	echo "Adding source line to .${shell}rc"
+	echo "\nsource $HOME/.stytch-cli-completion.$shell" >>$HOME/.${shell}rc
+fi
+
+echo
+echo "Installation complete. Run 'source $HOME/.${shell}rc' or restart your terminal to enable shell completions."

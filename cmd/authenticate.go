b495b3f997315b59faac96438e4173dfbceef27e
Jack Cook <jcook@stytch.com>
2025-05-07 14:01:12 -0700

    Generate PKCE pair for auth flows.

--- cmd/authenticate.go
+++ cmd/authenticate.go
@@ -10,18 +10,17 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/stytchauth/stytch-cli/cmd/internal"
 	"github.com/stytchauth/stytch-cli/utils"
 
 	"github.com/spf13/cobra"
 )
 
 const (
-	PortUrl       = "127.0.0.1:5001"
-	ClientId      = "connected-app-live-d552cc32-a785-4371-bf85-0af85f5f7067"
-	CodeChallenge = "dcvJOywUwb5HOWOyhmOI5dSc4_VHQU8Xkp9bXD-tGWI"
-	CodeVerifier  = "00afa9f459ce29bd4f9cd89f3a26036c3a2a772abd929e3fe179cb41"
-	ProjectId     = "project-live-0f74ccf8-79bd-4096-bd3f-5317c0e69a3b"
-	Scopes        = "openid email profile project"
+	PortUrl   = "127.0.0.1:5001"
+	ClientId  = "connected-app-live-d552cc32-a785-4371-bf85-0af85f5f7067"
+	ProjectId = "project-live-0f74ccf8-79bd-4096-bd3f-5317c0e69a3b"
+	Scopes    = "openid email profile project"
 )
 
 func NewAuthenticateCommand() *cobra.Command {
@@ -31,9 +30,12 @@ func NewAuthenticateCommand() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			stop := make(chan struct{})
 
+			// Generate PKCE pair.
+			verifier, challenge := internal.PKCECodePair()
+
 			mux := http.NewServeMux()
 			mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				handleCallback(w, r)
+				handleCallback(w, r, verifier)
 				stop <- struct{}{}
 			})
 
@@ -56,8 +58,8 @@ func NewAuthenticateCommand() *cobra.Command {
 			params.Add("response_type", "code")
 			params.Add("client_id", ClientId)
 			params.Add("redirect_uri", fmt.Sprintf("http://%s", PortUrl))
-			params.Add("code_challenge", CodeChallenge)
-			params.Add("code_verifier", CodeVerifier)
+			params.Add("code_verifier", verifier)
+			params.Add("code_challenge", challenge)
 			params.Add("scope", Scopes)
 			u.RawQuery = params.Encode()
 
@@ -78,7 +80,7 @@ func NewAuthenticateCommand() *cobra.Command {
 	return command
 }
 
-func handleCallback(w http.ResponseWriter, r *http.Request) {
+func handleCallback(w http.ResponseWriter, r *http.Request, pkceVerifier string) {
 	code := r.URL.Query().Get("code")
 	if code == "" {
 		http.Error(w, "Missing code parameter", http.StatusBadRequest)
@@ -86,7 +88,7 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Printf("✅ Received code: %s\n", code)
 
-	accessToken := getAccessTokenFromCode(code)
+	accessToken := getAccessTokenFromCode(code, pkceVerifier)
 	// Save the access token securely
 	err := utils.SaveToken(accessToken)
 	if err != nil {
@@ -102,7 +104,7 @@ type GetAccessTokenResp struct {
 	AccessToken string `json:"access_token"`
 }
 
-func getAccessTokenFromCode(code string) string {
+func getAccessTokenFromCode(code string, pkceVerifier string) string {
 	// make request to stytch with code to get access token
 	// store the access token/refresh token locally
 	tokenUrl := fmt.Sprintf("https://api.ollie.dev.stytch.com/v1/public/%s/oauth2/token", ProjectId)
@@ -111,7 +113,7 @@ func getAccessTokenFromCode(code string) string {
 		"redirect_uri":  fmt.Sprintf("http://%s", PortUrl),
 		"grant_type":    "authorization_code",
 		"code":          code,
-		"code_verifier": CodeVerifier,
+		"code_verifier": pkceVerifier,
 	}
 
 	bodyBytes, err := json.Marshal(requestBody)

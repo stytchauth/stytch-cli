499ad33be323b84fb514a484a45180b6c428e0c5
Jack Cook <jcook@stytch.com>
2025-05-07 10:46:24 -0700

    Minor cleanup

--- cmd/internal/clientutils.go
+++ cmd/internal/clientutils.go
@@ -2,22 +2,17 @@ package internal
 
 import (
 	"log"
+	"sync"
 
 	"github.com/stytchauth/stytch-management-go/v2/pkg/api"
 
 	"github.com/stytchauth/stytch-cli/utils"
 )
 
-var client *api.API
-
-func SetDefaultMangoClient(c *api.API) {
-	client = c
-}
-
-func GetDefaultMangoClient() *api.API {
+var MangoClient = sync.OnceValue(func() *api.API {
 	token, err := utils.LoadToken()
 	if err != nil {
 		log.Fatal("Unable to load access token: ", err)
 	}
 	return api.NewAccessTokenClient(token)
-}
+})
